4313deff18d2a865eb35a65fcc595baa3cf97675
hanzeil <tang5266868@gmail.com>
2015-04-12 16:13:12 +0800

    load file as chunks but not a whole file(in mem)

--- saveFile.php
+++ saveFile.php
@@ -2,8 +2,27 @@
 $filename   = $_SERVER['HTTP_X_FILE_NAME'];
 $filesize   = $_SERVER['HTTP_X_FILE_SIZE'];
 $dir="uploads/" . $_SERVER['HTTP_X_DIR_NAME'] . '/';
+//error
+  switch( $_FILES['file']['error'] ) {
+            case UPLOAD_ERR_OK:
+                $message = false;;
+                break;
+            case UPLOAD_ERR_INI_SIZE:
+            case UPLOAD_ERR_FORM_SIZE:
+                $message .= ' - file is too large. ';
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $message .= ' - file upload was not completed.';
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $message .= ' - zero-length file uploaded.';
+                break;
+            default:
+                $message .= ' - internal error #'.$_FILES['newfile']['error'];
+                break;
+        }
 // name must be in proper format
-if (isset($_SERVER['HTTP_X_FILE_NAME'])) {
+if (!$message) {
 	// we store chunks in directory named after filename
 	if (!file_exists("uploads/")){
 		mkdir("uploads/");
@@ -12,10 +31,12 @@ if (isset($_SERVER['HTTP_X_FILE_NAME'])) {
 		mkdir($dir);
 	}
 	$target = $dir . $filename;
-	move_uploaded_file($_FILES["file"]["tmp_name"], $target);
+	if(!move_uploaded_file($_FILES["file"]["tmp_name"], $target)){
+        $message = 'Error uploading file - could not save upload (this will probably be a permissions problem in '.$dest.')';
+    } 
+    else{
+        $message = 'File uploaded okay.';
+    }
 }
-else{
-    throw new Exception('Name required');
-}
-
+echo $message;
 
--- sliceFile.js
+++ sliceFile.js
@@ -1,93 +1,84 @@
-//open the will-be-sliced file
-function loadClientFile(url,callback){
-	var xhr=new XMLHttpRequest();
-	xhr.open('GET',url, true);
-	xhr.responseType='arraybuffer';
-	xhr.send();
-	xhr.onload=function(){
-		if(xhr.status!=200){
-			alert("load original file error");
-			return false;
-		} 
-		callback(new Uint8Array(xhr.response)); //slice and upload
-	}
-}
 //entry function
 function readySlice(){
-	var url=new String(document.getElementById('url').value);
-	var chunkSize=1024*document.getElementById('chunkSize').value;
 	//get the filename of the URL
-	var fileName=url.substr(url.lastIndexOf('\\')+1);
-	var fileName=fileName.substr(url.lastIndexOf('/')+1);
-	var fileName=fileName.substring(0,url.lastIndexOf('.'));
-	//send chunk and create json array
-	loadClientFile(url,function(response){
-		var file=new Blob([response],{type: 'video/webm'});
-		var chunkNum=Math.ceil(file.size / chunkSize);
-		//json
-		var fileJson={};
-		fileJson["fileName"]=fileName+".webm"; 
-		fileJson["chunkJsons"]=[];
-
-		//send chunk
-		var i=0;
-		(function sendChunk(i){
-			var startByte=chunkSize*i;
-			var chunk=file.slice(startByte,startByte+chunkSize);
-			var chunkName=fileName+i+".webm";
-			//write json
-			var chunkJson={};
-			chunkJson["chunkName"]="uploads/"+fileName+"/"+chunkName;
-			chunkJson["chunkOffset"]=startByte;
-			chunkJson["chunkSize"]=chunkSize;
-			fileJson["chunkJsons"].push(chunkJson);
-			//send chunk to server 
-			url="saveFile.php";   //server url
-			var xhr=new XMLHttpRequest();
-			xhr.open('POST',url,true);
-			xhr.setRequestHeader("X-File-Name",chunkName);
-			xhr.setRequestHeader("X-File-Size",chunk.size);
-			xhr.setRequestHeader("X-Dir-Name",fileName);
-			//xhr.setRequestHeader("Content-Type","multipart/form-data");
-			var formdata=new FormData(); 
-			formdata.append("file",chunk);
-			xhr.send(formdata);
-			xhr.onload=function(){
-				if(xhr.status!=200){
-					alert("send chunk error");
-				}
-				if(i<chunkNum-1){
-					i++;
-					sendChunk(i);
-				}
-				else{
-					alert("chunk send successfully")
-					//upload json
-					var jsonString = JSON.stringify(fileJson);
-					var jsonName=fileName+".json";
-					uploadJson(jsonString,jsonName);
-				}
-			}
-		}
-		)(i);
-	}
-	);
-}
-function uploadJson(jsonString,jsonName){
-url="saveJson.php";
-var xhr=new XMLHttpRequest();
-xhr.open("POST",url,true);
-var formdata=new FormData();
-xhr.setRequestHeader("X-File-Name",jsonName);
-formdata.append("json",jsonString);
-xhr.send(formdata);
-xhr.onload=function(){
-	if(xhr.status!=200){
-		alert("send json error");
+	var file=document.getElementById('file').files[0];
+	var chunkSize=1024*document.getElementById('chunkSize').value;
+	var fileName=file.name;
+	var fileType=fileName.substr(fileName.lastIndexOf('.')+1);
+	var fileName=fileName.substring(0,fileName.lastIndexOf('.'));
+	//judge the type of the media file
+	if(fileType=="webm"){
+		sliceWebm(file,fileName,chunkSize);
 	}
 	else{
-		//alert(xhr.response);
-		alert("json send successfully");
+		alert("file type is not supported");
 	}
 }
+//send chunk and create json array
+function sliceWebm(file,fileName,chunkSize){
+	var chunkNum=Math.ceil(file.size / chunkSize);
+	//json
+	var fileJson={};
+	fileJson["fileName"]=fileName+".webm"; 
+	fileJson["chunkJsons"]=[];
+
+	//send chunk
+	var i=0;
+	(function sendChunk(i){
+		var startByte=chunkSize*i;
+		var chunk=file.slice(startByte,startByte+chunkSize);
+		var chunkName=fileName+i+".webm";
+		//write json
+		var chunkJson={};
+		chunkJson["chunkName"]="uploads/"+fileName+"/"+chunkName;
+		chunkJson["chunkOffset"]=startByte;
+		chunkJson["chunkSize"]=chunkSize;
+		fileJson["chunkJsons"].push(chunkJson);
+		//send chunk to server 
+		url="saveFile.php";   //server url
+		var xhrSend=new XMLHttpRequest();
+		xhrSend.open('POST',url,true);
+		xhrSend.setRequestHeader("X-File-Name",chunkName);
+		xhrSend.setRequestHeader("X-File-Size",chunk.size);
+		xhrSend.setRequestHeader("X-Dir-Name",fileName);
+		//xhr.setRequestHeader("Content-Type","multipart/form-data");
+		var formdata=new FormData(); 
+		formdata.append("file",chunk);
+		xhrSend.send(formdata);
+		xhrSend.onload=function(){
+			if(xhrSend.status!=200){
+				alert("send chunk error");
+			}
+			if(i<chunkNum-1){
+				i++;
+				sendChunk(i);
+			}
+			else{
+				alert("send chunks successfully")
+				//upload json
+				var jsonString = JSON.stringify(fileJson);
+				var jsonName=fileName+".json";
+				uploadJson(jsonString,jsonName);
+			}
+		}
+	}
+	)(i);
+}
+function uploadJson(jsonString,jsonName){
+	url="saveJson.php";
+	var xhr=new XMLHttpRequest();
+	xhr.open("POST",url,true);
+	var formdata=new FormData();
+	xhr.setRequestHeader("X-File-Name",jsonName);
+	formdata.append("json",jsonString);
+	xhr.send(formdata);
+	xhr.onload=function(){
+		if(xhr.status!=200){
+			alert("send json-file error");
+		}
+		else{
+			//alert(xhr.response);
+			alert("send json-files successfully");
+		}
+	}
 }
