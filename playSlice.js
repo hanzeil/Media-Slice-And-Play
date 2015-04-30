bb85aac2835f7f3b10f605049c33188939fc3ec2
hanzeil <tang5266868@gmail.com>
2015-04-30 11:10:30 +0800

    a little bug

--- playSlice.js
+++ playSlice.js
@@ -23,7 +23,7 @@ function loadJson(jsonFile, callback) {
 	xhr.send();
 	xhr.onload = function() {
 		if (xhr.status != 200) {
-			log.e("Load json file error");
+			Log.e("Load json file error");
 			alert("Load json file error");
 			return false;
 		}
